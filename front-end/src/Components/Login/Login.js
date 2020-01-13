45cf673941754d69f6bfaeba846777c923e05e12
angel arias <angelarias105@gmail>
2020-01-13 01:14:01 -0500

    working on the responsive navbar with the burger menu for mobile devices

--- front-end/src/Components/Login/Login.js
+++ front-end/src/Components/Login/Login.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 import "./Login.css";
 
+
 class Login extends Component {
   constructor(props) {
     super(props);
--- front-end/src/Components/Navigation/Navigation.js
+++ front-end/src/Components/Navigation/Navigation.js
@@ -6,35 +6,42 @@ const Navigation = () => {
   return (
     <React.Fragment>
       <div className="nav-container">
-      <div className="logo-container">
-        <Link className="logoName" to="/">
-          Creditfly
-        </Link>
-      </div>
-      <nav className="main-nav">
-        <ul className="links-list">
-          <li>
-            <Link className="nav-links" to="/about">
-              About Us
-            </Link>
-          </li>
-          <li>
-            <Link className="nav-links" to="/rewards">
-              Rewards
-            </Link>
-          </li>
-          <li>
-            <Link className="nav-links" to="/register">
-              Register
-            </Link>
-          </li>
-          <li>
-            <Link className="nav-links" to="/login">
-              Login
-            </Link>
-          </li>
-        </ul>
-      </nav>
+        <div className="logo-container">
+          <Link className="logoName" to="/">
+            Creditfly
+          </Link>
+        </div>
+        <nav className="main-nav">
+          <div className="links-list">
+            <ul>
+              <li>
+                <Link className="nav-links" to="/about">
+                  About Us
+                </Link>
+              </li>
+              <li>
+                <Link className="nav-links" to="/rewards">
+                  Rewards
+                </Link>
+              </li>
+              <li>
+                <Link className="nav-links" to="/register">
+                  Register
+                </Link>
+              </li>
+              <li>
+                <Link className="nav-links" to="/login">
+                  Login
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="burger">
+              <div className="line1"></div>
+              <div className="line2"></div>
+              <div className="line3"></div>
+            </div>
+        </nav>
       </div>
     </React.Fragment>
   );
